3fa1851f26d16e73b86fb1f2d6ccde42171f4d4b
Kai Haase <kai.haase@lenne.tech>
2021-10-27 05:20:32 +0200

    Formula for sshpass

---
+++ formulas/sshpass.rb
@@ -0,0 +1,18 @@
+# Inspired by https://github.com/kadwanev/bigboybrew/blob/master/Library/Formula/sshpass.rb
+require 'formula'
+
+class Sshpass < Formula
+  url 'http://sourceforge.net/projects/sshpass/files/sshpass/1.09/sshpass-1.09.tar.gz'
+  homepage 'http://sourceforge.net/projects/sshpass'
+  sha256 'cbc0e1300c7f1cacf9c850e5b42773cb53211d9d'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make install"
+  end
+
+  def test
+    system "sshpass"
+  end
+end
